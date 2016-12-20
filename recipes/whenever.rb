39e4f8bb1457d13251053e4d04a6e4bfe57c665f
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2016-12-20 12:28:36 +0100

    another workaround for fixing wrong path schedule

--- recipes/whenever.rb
+++ recipes/whenever.rb
@@ -13,7 +13,8 @@ end
 
 every_enabled_application do |app|
   deploy_to = deploy_dir(app)
-  release_path = Dir[File.join(deploy_to, 'releases', '*')].sort.last
+  releases = Dir[File.join(deploy_to, 'releases', '*')].map { |fp| fp.split('/').last }
+  release_path = File.join(deploy_to, 'releases', releases.map(&:to_i).max.to_s)
 
   execute 'create new crontabs from whenever gem' do
     command 'RAILS_ENV=production bin/bundle exec whenever --update-crontab'
